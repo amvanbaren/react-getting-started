17b6b7e1b0d316940d0e5e94da174105f981092b
Aart van Baren <amvanbaren@hotmail.com>
2017-06-18 12:17:36 +0200

    set name

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+  formatName () {
+    return this.props.firstName + ' ' + this.props.lastName
+  }
+
   render () {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Hello World!</h1>
+          <h1>Hello {this.formatName()}</h1>
         </div>
       </div>
     )
